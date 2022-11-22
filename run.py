7d3d1622dbf33bdd7a8557257ba6fa619165f3e3
Nicole MN.E <mcnultyengstrom@gmail.com>
2022-11-22 23:33:30 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,114 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+HANGMANGRAPHICS = [
+        """
+        ___________
+        |/        |
+        |         O
+        |        /|\\
+        |         |
+        |        / \\
+        |\\
+        ========
+        """,
+        """
+        ___________
+        |/        |
+        |         O
+        |        /|\\
+        |         |
+        |        /
+        |\\
+        ========
+        """,
+        """
+        __________
+        |/        |
+        |         O
+        |        /|\\
+        |         |
+        |
+        |\\
+        ========
+        """,
+        """
+        __________
+        |/        |
+        |         O
+        |        /|
+        |         |
+        |
+        |\\
+        ========
+        """,
+        """
+        __________
+        |/        |
+        |         O
+        |         |
+        |         |
+        |
+        |\\
+        ========
+        """,
+        """
+        __________
+        |/        |
+        |         O
+        |
+        |
+        |
+        |\\
+        ========
+        """,
+        """
+        __________
+        |/
+        |
+        |
+        |
+        |
+        |\\
+        ========
+        """,
+        """
+        __________
+        |/
+        |
+        |
+        |
+        |
+        |
+        ========
+        """,
+        """
+        |/
+        |
+        |
+        |
+        |
+        |
+        ========
+        """,
+
+        """
+        |
+        |
+        |
+        |
+        |
+        ========
+        """,
+        """
+        """
+]
+
+# Capital cities word bank
+words = ('dublin reykjavik cardiff edinburgh belfast london oslo stockholm '
+         'helsinki berlin copenhagen amsterdam brussels paris '
+         'luxembourg lisbon madrid rome bern vienna warsaw '
+         'prague bratislava vilnius riga tallinn bucharest budapest '
+         'zagreb sarajevo ljubljana belgrade skopje sofia pristina '
+         'minsk kiev tbilisi athens ankara vaduz podgorica valetta tirana '
+         'nicosia chisinau moscow monaco tokyo seoul bejing kathmandu cairo '
+         'canberra hanoi lima ottawa jerusalem santiago brasilia kingston '
+         'havana wellington pyongyang taipei bangkok manila jakarta '
+         'mogadishu ulaanbataar ').split()
