0510f85fdcd259698de98c85b2515a81731bf11a
Nicole MN.E <mcnultyengstrom@gmail.com>
2022-11-23 00:49:12 +0000

    Add start_options function to give user choice at start

--- run.py
+++ run.py
@@ -114,13 +114,32 @@ words = ('dublin reykjavik cardiff edinburgh belfast london oslo stockholm '
          'mogadishu ulaanbataar ').split()
 
 
+def start_options():
+    print("   Press " + "S  " + "to start new game")
+    print("   Press " + "H  " + "for instructions")
+    
+    options = False
+    while not options:
+        choice = input("\n ").upper()
+        if choice == "S":
+            options = True
+            print("  Starting new game...")
+
+        elif choice == "H":
+            options = True
+            print("  Showing instructions...")
+
+        else:
+            print("  Please select S or H to make your choice")
+            break
+
+
 def intro_title():
     print("""
     █░░ █▀▀ ▀█▀ ▀ █▀   █▀█ █░░ ▄▀█ █▄█  
     █▄▄ ██▄ ░█░ ░ ▄█   █▀▀ █▄▄ █▀█ ░█░ ▄ ▄ ▄
     """)
     print("""
-
     ██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
     ██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
     ███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
@@ -134,7 +153,8 @@ def main():
     intro_title()
     print(hangman_images[0])
     print('\n')
-    print("Welcome to Hangman!\nPlease select one of the choices below.")
+    print("   Welcome to Hangman!\n")
+    start_options()
 
 
 main()
