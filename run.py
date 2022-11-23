ebf085b9911c87d7682fb1c96478d70a93ec158c
Nicole MN.E <mcnultyengstrom@gmail.com>
2022-11-23 23:53:59 +0000

    Add random choice of word function

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import random
+
 hangman_images = [
         """
         ___________
@@ -125,22 +127,59 @@ hangman_intro_image = [
         """
 ]
 
+
+def random_city():
+    hidden_word = random.choice(cities)
+    print(hidden_word.upper())
+
+
 # Capital cities word bank
-words = ('dublin reykjavik cardiff edinburgh belfast london oslo stockholm '
-         'helsinki berlin copenhagen amsterdam brussels paris '
-         'luxembourg lisbon madrid rome bern vienna warsaw '
-         'prague bratislava vilnius riga tallinn bucharest budapest '
-         'zagreb sarajevo ljubljana belgrade skopje sofia pristina '
-         'minsk kiev tbilisi athens ankara vaduz podgorica valetta tirana '
-         'nicosia chisinau moscow monaco tokyo seoul bejing kathmandu cairo '
-         'canberra hanoi lima ottawa jerusalem santiago brasilia kingston '
-         'havana wellington pyongyang taipei bangkok manila jakarta '
-         'mogadishu ulaanbataar ').split()
+cities = ('dublin reykjavik cardiff edinburgh belfast london oslo stockholm '
+          'helsinki berlin copenhagen amsterdam brussels paris '
+          'luxembourg lisbon madrid rome bern vienna warsaw '
+          'prague bratislava vilnius riga tallinn bucharest budapest '
+          'zagreb sarajevo ljubljana belgrade skopje sofia pristina '
+          'minsk kiev tbilisi athens ankara vaduz podgorica valetta tirana '
+          'nicosia chisinau moscow monaco tokyo seoul bejing kathmandu cairo '
+          'canberra hanoi lima ottawa jerusalem santiago brasilia kingston '
+          'havana wellington pyongyang taipei bangkok manila jakarta '
+          'mogadishu ulaanbataar ').split()
+
+
+def intro_title():
+    """
+    Function to display "Let's play Hangman" text when running the program
+    """
+    print("""
+    █░░ █▀▀ ▀█▀ ▀ █▀   █▀█ █░░ ▄▀█ █▄█ 
+    █▄▄ ██▄ ░█░ ░ ▄█   █▀▀ █▄▄ █▀█ ░█░ ▄ ▄ ▄
+    """)
+    print("""
+    ██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
+    ██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
+    ███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
+    ██╔══██║██╔══██║██║╚████║██║░░╚██╗██║╚██╔╝██║██╔══██║██║╚████║
+    ██║░░██║██║░░██║██║░╚███║╚██████╔╝██║░╚═╝░██║██║░░██║██║░╚███║
+    ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
+    """)
+
+
+def start_game():
+    correct_guess = ''
+    wrong_guess = ''
+
+    print("\n")
+    print("\n")
+
+    # testing
+    return random_city()
 
 
 def start_options():
-    # print("   Press " + "» S  " + "to start new game")
-    # print("   Press " + "» H  " + "for instructions")
+    """
+    Function to display user choice to either 
+    start game or view instructions
+    """
     print(hangman_intro_image[0])
     
     options = False
@@ -156,15 +195,19 @@ def start_options():
 
         else:
             print(f" You selected {choice}. "
-            "Please select S or H to continue ♥")
+                  "Please select S or H to continue ♥")
 
 
 def instructions():
+    """
+    Function to display help instructions for game rules.
+    Starts the game when user enters S
+    """
     print("\n")
     print("A name of a city will be hidden behind blank spaces.\n"
-    "You must find the correct letters by guessing every letter.\n"
-    "Correct guesses will reveal a letter in the name.\nWrong guesses "
-    "reduces a life. You have 7 lives.\n\nHope you have fun!")
+          "You must find the correct city by guessing each letter.\n"
+          "Correct guesses will reveal a letter in the name.\nWrong guesses "
+          "will reduce a life. You have 7 lives.\n\nHope you have fun!")
     print("\nPlease select S to play ♥")
 
     options = False
@@ -176,21 +219,7 @@ def instructions():
 
         else:
             print(f" You selected {choice}. "
-            "Please select S play ♥")
-
-def intro_title():
-    print("""
-    █░░ █▀▀ ▀█▀ ▀ █▀   █▀█ █░░ ▄▀█ █▄█ 
-    █▄▄ ██▄ ░█░ ░ ▄█   █▀▀ █▄▄ █▀█ ░█░ ▄ ▄ ▄
-    """)
-    print("""
-    ██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
-    ██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
-    ███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
-    ██╔══██║██╔══██║██║╚████║██║░░╚██╗██║╚██╔╝██║██╔══██║██║╚████║
-    ██║░░██║██║░░██║██║░╚███║╚██████╔╝██║░╚═╝░██║██║░░██║██║░╚███║
-    ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
-    """)
+                  "Please select S play ♥")
 
 
 def main():
